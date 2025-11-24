0d2f913647ea5399a94282e9f33f89313a8cb131
Miguel-Andrade-Cruz <angelium.eddra1972@gmail.com>
2025-11-24 20:03:18 -0300

    Redo 'convert' and 'x_t-_decimal' logic and rearrenge import tree and libraries

--- conversao-bases/src/convert.c
+++ conversao-bases/src/convert.c
@@ -1,5 +1,6 @@
 #include <math.h>
-#include "wrap_function.c"
+#include <string.h>
+#include "gui.c"
 
 
 void splitter(ConversionPackage *package, int preserve_base, int returned) {
@@ -13,8 +14,8 @@ void x_to_decimal(ConversionPackage *package) {
     
     int alg_limit = package->size;
     int converted = 0;
-    for (int i = 0, *alg_pos = &i; i < alg_limit; i++) {
-        converted += pow(package->target_base, *alg_pos) * package->current_number[i];
+    for (int i = alg_limit - 1, exponent = 0; i >= 0; i--, exponent++) {
+        converted += pow(package->target_base, exponent) * package->current_number[i];
     }
     package->base_10_number = converted;
     
@@ -27,7 +28,7 @@ void decimal_to_x(ConversionPackage *package) {
     int alg_limit = package->size;
     int value = package->base_10_number;
     
-    int i = alg_limit -1;
+    int i = alg_limit - 1;
     while (value != 0) {
         package->target_number[i] = value % package->target_base;
         value /= package->target_base;
@@ -63,8 +64,6 @@ void bin_to_dec_partition(ConversionPackage *package, int chunk_size) {
         }
         
         x_to_decimal(&transition_package);
-        package->current_number[chunk_index] = transition_package.base_10_number;
-        
     }
     
     return;
@@ -75,10 +74,61 @@ void bin_to_dec_partition(ConversionPackage *package, int chunk_size) {
 
 void convert(ConversionPackage *package) {
     
+    if (package->target_base == 10) {
+        
+        ConversionPackage temp_package = {
+            .target_base = 10,
+            .size = package->size
+        };
+        x_to_decimal(package);
+        decimal_to_x(&temp_package);
+        memcpy(package->target_number, temp_package.target_number, sizeof(temp_package.target_number));
+    
+        return;
+    }
+
+    
+    if (package->current_base == 8 || package->current_base == 16) {
+        x_to_decimal(package);
+        
+        ConversionPackage temp_package = {
+            .base_10_number = package->base_10_number,
+            .size = package->size,
+            .target_base = 2,
+        };
+        decimal_to_x(&temp_package);
+        
+        memcpy(package->target_number, temp_package.target_number, sizeof(temp_package.target_number));
+        if (package->target_base == 8) {
+            bin_to_dec_partition(package, 3);
+        }
+        if (package->target_base == 16) {
+            bin_to_dec_partition(package, 4);
+        }
+    }
+    
+    
+    if (package->current_base == 2) {
+        if (package->target_base == 8) {
+            bin_to_dec_partition(package, 3);
+        }
+        if (package->target_base == 16) {
+            bin_to_dec_partition(package, 4);
+        }
+    }
+    
     
     if (package->current_base == 10) {
+        ConversionPackage temp_package = {
+            .current_number = *package->current_number,
+            .current_base = 10,
+            .size = package->size
+        };
         
-        x_to_decimal(package);
+        x_to_decimal(&temp_package);
+        package->base_10_number = temp_package.base_10_number;
+        decimal_to_x(package);
     }
+
     return;
 }
