144eaf30fd9da63b2da4d89b6d1b94261f8affb4
Miguel-Andrade-Cruz <angelium.eddra1972@gmail.com>
2025-11-24 18:35:47 -0300

    rearrenge import tree and complete program flow

--- conversao-bases/src/main.c
+++ conversao-bases/src/main.c
@@ -1,14 +1,13 @@
 #include "convert.c"
-#include "gui.c"
 
 int main() {
-
+    
     ConversionPackage conversion_package;
 
     pack_input(&conversion_package);
-
-    chunk_conversion(&conversion_package, 4);
-
-    printf("%s", conversion_package.target_number);
+    convert(&conversion_package);
+    
+    show_output(&conversion_package);
+    
     return 0;
 }
