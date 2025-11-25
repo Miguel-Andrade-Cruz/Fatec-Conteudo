8a0d40184ca955205ad5871ab7c06c52e667a301
Miguel-Andrade-Cruz <angelium.eddra1972@gmail.com>
2025-11-25 17:25:47 -0300

    Add 'slide_left' function to clean leading zeros

--- conversao-bases/src/gui.c
+++ conversao-bases/src/gui.c
@@ -16,14 +16,34 @@ void get_number_to_convert_message() {
     return;
 }
 
+void slide_left(char *array) {
+    
+    int offset = 0;
+    while (array[offset] == '0') {
+        offset++;
+    }
+    
+    int i = 0;
+    while (array[i + offset] != '\0') {
+        array[i] = array[i + offset];
+        i++;
+    }
+    array[i] = '\0';
+    
+    return;
+}
+
 
 void show_output(ConversionPackage *package) {
 
-    char current_char_output[LIMIT_MAX];
-    char target_char_output[LIMIT_MAX];    
+    char current_char_output[LIMIT_MAX + 1]; // +1 for null terminator
+    char target_char_output[LIMIT_MAX + 1]; // +1 for null terminator
      
     ints_to_chars(package->current_number, current_char_output);
     ints_to_chars(package->target_number, target_char_output);
+    
+    slide_left(current_char_output);
+    slide_left(target_char_output);
     
     printf(
         "\n Número: %s  Base numérica: %d \n Número: %s  Base numérica: %d \n",
