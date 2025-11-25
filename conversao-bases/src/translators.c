ca3044f15530849e6b1ff58f6e49c5055fccafb5
Miguel-Andrade-Cruz <angelium.eddra1972@gmail.com>
2025-11-25 17:26:29 -0300

    Fix null terminator issue and bitmask offset problems

--- conversao-bases/src/translators.c
+++ conversao-bases/src/translators.c
@@ -1,25 +1,27 @@
 #include "wrap_type.c"
+#include <string.h>
 
 // Resolve null character for both conversions
 
 
 int char_to_int(char character) {
     
-    int BITMASK = 0b00001111;
-
+    const int BITMASK = 0x0F;
+    if (character >= 'A') {
+        return character - 'A' + 10;
+    }
     return character & BITMASK;
 }
 
+
 char int_to_char(int integer) {
     
-    int NUMERIC_BITMASK = 0b00110000;
-    int ALPHA_BITMASK = 0b01000000;
+    const int NUMERIC_BITMASK = 0x30;
+    const int ALPHA_BITMASK = 0x40;
     
-    if (integer < 9) return integer | NUMERIC_BITMASK;
-    
-    return integer | ALPHA_BITMASK;
+    if (integer < 10) return integer | NUMERIC_BITMASK;
+    return (integer - 9) | ALPHA_BITMASK;
 }
-
 
 
 void chars_to_ints(char *before_vector, int *after_vector) {
@@ -32,11 +34,13 @@ void chars_to_ints(char *before_vector, int *after_vector) {
 }
 
 
-void ints_to_chars(int *before_vector, char *afer_vector) {
+void ints_to_chars(int *before_vector, char *after_vector) {
     
     for (int i = 0; i < LIMIT_MAX; i++) {
         
-        afer_vector[i] = int_to_char(before_vector[i]);
+        after_vector[i] = int_to_char(before_vector[i]);
     }
+    after_vector[LIMIT_MAX + 1] = '\0';
+    
     return;
 }
