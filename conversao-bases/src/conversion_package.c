4047649db2d40fd4a72dcfa54a60e02ad7c81189
Miguel-Andrade-Cruz <angelium.eddra1972@gmail.com>
2025-11-27 15:47:18 -0300

    Rename 'wrap_type' to 'conversion_package'

---
+++ conversao-bases/src/conversion_package.c
@@ -0,0 +1,17 @@
+#include "alg_limits.c"
+
+
+typedef struct {
+    
+    int current_base;
+    int target_base;
+    
+    int current_number[LIMIT_MAX];
+    
+    int target_number[LIMIT_MAX];
+    
+    int base_10_number;
+    
+    ALG_LIMIT size;
+
+} ConversionPackage;
