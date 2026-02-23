b9ad715ad935f197ff6b588ed4fb90f39fd6f777
Miguel-Andrade-Cruz <angelium.eddra1972@gmail.com>
2026-02-23 19:34:43 -0300

    Rename and create exercise files for organization

---
+++ Miguel/Exercícios/5__conversor_segundos.c
@@ -0,0 +1,10 @@
+// 5 - Escreva um programa que receba via teclado um tempo em segundos e converta
+//      para horas, minutos e segundos.
+//      Ex.: recebe 3850 segundos que converte para 1h 4min 10s.
+
+int main() {
+	
+
+
+
+}
