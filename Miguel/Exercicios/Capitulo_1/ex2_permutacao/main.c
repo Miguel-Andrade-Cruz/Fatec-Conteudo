25332f08352235b05a3214ac88c7b09845e30e5a
Miguel-Andrade-Cruz <angelium.eddra1972@gmail.com>
2026-03-10 20:16:17 -0300

    add "stdio.h" lib and loop for continuous user input

--- Miguel/Exercicios/Capitulo_1/ex2_permutacao/main.c
+++ Miguel/Exercicios/Capitulo_1/ex2_permutacao/main.c
@@ -1,16 +1,21 @@
+#include <stdio.h>
 #include "middleware.h"
 
 
 int main() {
 
-    char var_type = select_var_type();
-    
-    if (var_type == 'c') {
-        char_switcher();
-    }
-    else if (var_type == 'i') {
-        int_switcher();
-    }
+    char again = 'n';
+    do {
+        char var_type = select_var_type();
 
+        if (var_type == 'c') {
+            char_switcher();
+        }
+        else if (var_type == 'i') {
+            int_switcher();
+        }
+
+        scanf("%c", &again);
+} while (again == 's');
     return 0;
 }
