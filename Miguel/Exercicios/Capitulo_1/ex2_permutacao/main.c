691ac79ca38539c4b83afb00495482b995cbaca4
Miguel-Andrade-Cruz <angelium.eddra1972@gmail.com>
2026-03-10 20:26:59 -0300

    Add loop for continuous usage

--- Miguel/Exercicios/Capitulo_1/ex2_permutacao/main.c
+++ Miguel/Exercicios/Capitulo_1/ex2_permutacao/main.c
@@ -15,7 +15,8 @@ int main() {
             int_switcher();
         }
 
+        printf("Deseja executar novamente? ");
         scanf("%c", &again);
-} while (again == 's');
+    } while (again == 's');
     return 0;
 }
