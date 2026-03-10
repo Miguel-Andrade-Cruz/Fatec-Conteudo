ac7914919dae54f06f2535f96e704037267e6386
Miguel-Andrade-Cruz <angelium.eddra1972@gmail.com>
2026-03-10 19:24:35 -0300

    Remodel of lib usage and add all scripts into one for file import debug

--- minhas-bibliotecas/calid/test/test_calid.c
+++ minhas-bibliotecas/calid/test/test_calid.c
@@ -1,39 +1,62 @@
-#include <stdio.h>
-#include "../src/calid.h"
-#include "../src/validators/custom.c"
+#include "../src/fill_form.c"
 
 int main() {
 
+
     char name[25], email[45], cpf[14];
 
-    RuleSet name_rules = {
-        capitalized(),
-        max_length(25)
+    Rule name_rules[2] = {
+        {
+            .validator = max_length,
+            .bound.value.a_int = 25,
+            .bound.type = INT
+        },
+        {
+            .validator = capitalized,
+        }
     };
 
-    RuleSet email_rules = {
-            alphaNum_only(),
-            max_length(45)
+    Rule email_rules[2] = {
+            {
+                .validator = alphaNum_only,
+            },
+            {
+                .validator = max_length,
+                .bound.value.a_int = 45,
+                .bound.type = INT
+            }
     };
 
-    RuleSet cpf_rules = {
-            numeric_only(),
-            max_length(11)
-            // valid_cpf(),
+
+    Rule cpf_rules[2] = {
+        {
+            .validator = numeric_only,
+        },
+        {
+            .validator = max_length,
+            .bound.value.a_int = 14,
+            .bound.type = INT
+        }
     };
 
     FormField fields[3] = {
         {
+            .question = "What is your name?",
+            .requests = name_rules,
             .pointer = {.value.a_char = name, .type = CHAR},
-            .requests = &name_rules
+            .rules_qtd = 2
         },
         {
+            .question = "Enter your email:",
             .pointer = {.value.a_char = email, .type = CHAR},
-            .requests = &email_rules
+            .requests = email_rules,
+            .rules_qtd = 2
         },
         {
+            .question = "Enter your CPF:",
             .pointer = {.value.a_char = cpf, .type = CHAR},
-            .requests = &cpf_rules
+            .requests = cpf_rules,
+            .rules_qtd = 2
         }
     };
 
