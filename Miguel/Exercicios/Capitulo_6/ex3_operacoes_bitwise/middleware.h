66260785511bfacd44df84fa0228bf152d26cf13
Miguel-Andrade-Cruz <angelium.eddra1972@gmail.com>
2026-03-16 17:08:08 -0300

    CAP 6 EX 3
    Write middleware declaration an implementation

--- Miguel/Exercicios/Capitulo_6/ex3_operacoes_bitwise/middleware.h
+++ Miguel/Exercicios/Capitulo_6/ex3_operacoes_bitwise/middleware.h
@@ -2,3 +2,9 @@
 
 
 void clear_buffer();
+
+void ask_for_two_numbers(int *num_1, int *num_2);
+
+void print_bitwise_and(int num_1, int num_2);
+void print_bitwise_or(int num_1, int num_2);
+void print_bitwise_xor(int num_1, int num_2);
--- Miguel/Exercicios/Capitulo_6/ex3_operacoes_bitwise/midlleware.c
+++ Miguel/Exercicios/Capitulo_6/ex3_operacoes_bitwise/midlleware.c
@@ -6,3 +6,41 @@ void clear_buffer() {
     while(getchar() != '\n');
     return;
 }
+
+
+void ask_for_two_numbers(int *num_1, int *num_2) {
+    
+    printf("Insira o primeiro número:  ");
+    scanf("%d", num_1);
+    
+    printf("Insira o segundo número:  ");
+    scanf("%d", num_2);
+    
+    return;
+}
+
+void print_bitwise_and(int num_1, int num_2) {
+    
+    int bitwise_answer = num_1 & num_2;
+    
+    printf("Expressão: %d AND %d = %d(10) ou %x(16)", num_1, num_2, bitwise_answer, bitwise_answer);
+    return;
+}
+
+
+void print_bitwise_or(int num_1, int num_2) {
+    
+    int bitwise_answer = num_1 | num_2;
+    
+    printf("Expressão: %d OR %d = %d(10) ou %x(16)", num_1, num_2, bitwise_answer, bitwise_answer);
+    return;
+}
+
+
+void print_bitwise_xor(int num_1, int num_2) {
+    
+    int bitwise_answer = num_1 ^ num_2;
+    
+    printf("Expressão: %d XOR %d = %d(10) ou %x(16)", num_1, num_2, bitwise_answer, bitwise_answer);
+    return;
+}
