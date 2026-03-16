e00660c55ce25e4bf0444556e96bfe21a36cbfb5
Miguel-Andrade-Cruz <angelium.eddra1972@gmail.com>
2026-03-16 16:11:02 -0300

    Add ex 3 (untested)

---
+++ Miguel/Exercicios/Capitulo_6/ex3_operacoes_bitwise/main.c
@@ -0,0 +1,6 @@
+// 3 - Escreva um programa que receba via teclado 2 numeros inteiros e execute
+//    as operacoes logicas bit a bit AND, OU, OU EXCLUSIVO e imprima-os no
+//    video no formato decimal e hexadecimal.
+// 3 - Escreva um programa que receba via teclado 2 numeros inteiros e execute
+//    as operacoes logicas bit a bit AND, OU, OU EXCLUSIVO e imprima-os no
+//    video no formato decimal e hexadecimal.
