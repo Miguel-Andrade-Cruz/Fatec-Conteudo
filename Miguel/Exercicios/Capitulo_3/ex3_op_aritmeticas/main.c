39c93571fba9c58953ff050589a9fcb402262196
Miguel-Andrade-Cruz <angelium.eddra1972@gmail.com>
2026-03-16 16:05:45 -0300

    Add ex 3 (untested)

---
+++ Miguel/Exercicios/Capitulo_3/ex3_op_aritmeticas/main.c
@@ -0,0 +1,32 @@
+// 3 - Escreva um programa que receba na funcao main() 2 valores inteiro. Escreva
+//     uma funcao para cada operacoes aritmeticas e passe como parametro os 2
+//     valores recebidos na funcao main(). Retorne os resultados usando o comando
+//     return e imprima os 4 resultados no video na funcao main().
+
+#include "middleware.h"
+
+
+int main() {
+
+    char again = 'n';
+    do {
+
+        float sum_of, substraction_of, multiplication_of, division_of;
+
+        int num_1 = ask_first_number();
+        int num_2 = ask_second_number();
+
+        sum_of = add(num_1, num_2);
+        substraction_of = subtract(num_1, num_2);
+        multiplication_of = multiply(num_1, num_2);
+        division_of = divide(num_1, num_2);
+
+        show_aritmetics(sum_of, substraction_of, multiplication_of, division_of);
+
+        // +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-
+        printf("Deseja executar novamente? ( s / n )");
+        scanf("%c", &again);
+        clean_buffer();
+    } while (again == 's');
+    return 0;
+}
---
+++ Miguel/Exercicios/Capitulo_3/ex3_op_aritmeticas/middleware.h
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+
+void clean_buffer();
+
+int ask_first_number();
+int ask_second_number();
+
+float add(int num_1, int num_2);
+float subtract(int num_1, int num_2);
+float multiply(int num_1, int num_2);
+float divide(int num_1, int num_2);
+
+ void show_aritmetics(float sum, float subtraction, float multiplication, float division);
