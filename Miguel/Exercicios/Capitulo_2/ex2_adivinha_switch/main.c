92a1381150765129ed6d0e23650722aed71786db
Miguel-Andrade-Cruz <angelium.eddra1972@gmail.com>
2026-03-16 09:37:36 -0300

    Add ex 2 (untested)

---
+++ Miguel/Exercicios/Capitulo_2/ex2_adivinha_switch/main.c
@@ -0,0 +1,23 @@
+// 2 - Reescreva o programa do exercicio anterior agora utilizando o comando switch.
+//     Conte o n. de tentativas e imprima o resultado no video.
+
+#include "middleware.h"
+
+
+int main() {
+
+    char again = 'n';
+    do {
+        int tries;
+        
+        display_message();
+        tries = number_guesser();
+        
+        printf("Levaram %d tentativas para adivinhar", tries);
+        
+        printf("Deseja executar novamente? ");
+        scanf("%c", &again);
+    } while (again == 's');
+    return 0;
+
+}
---
+++ Miguel/Exercicios/Capitulo_2/ex2_adivinha_switch/middleware.c
@@ -0,0 +1,54 @@
+#include "middleware.h"
+#include <algorithm>
+
+
+void display_message() {
+    
+    printf("Bem vindo ao adivinhador!\n");
+    printf("Adivinharei seu número na menor quantidade de tentativas possível\n");
+    
+    return; //
+}
+
+int number_guesser() {
+    int guess;
+    int tries = 0;
+    int LOWER_BOUND = 1;
+    int UPPER_BOUND = 99;
+    
+    int half_cut = UPPER_BOUND - LOWER_BOUND / 2;
+    
+    char relation;
+    while (UPPER_BOUND - LOWER_BOUND == 2) {
+        printf("Seu número é maior que %d?  ", half_cut);
+        scanf("%c", &relation);
+        
+        switch (relation) {
+            case '>':
+                tries++;
+                LOWER_BOUND = half_cut;
+                half_cut = (UPPER_BOUND - LOWER_BOUND / 2) + LOWER_BOUND;
+            case '<':
+                tries++;
+                UPPER_BOUND = half_cut;
+                half_cut = (UPPER_BOUND - LOWER_BOUND / 2);
+            case '=':
+                guess = half_cut;
+                printf("O número escolhido é %d", guess);
+                return tries;
+        }
+    }
+    
+    printf("Seu número é %d?  ", LOWER_BOUND + 1);
+    scanf("%c", &relation);
+    switch (relation) {
+        case '=':
+            guess = LOWER_BOUND + 1;
+            printf("O número escolhido é %d", guess);
+        case '>':
+            tries++;
+            guess = UPPER_BOUND;
+            printf("O número escolhido é %d", guess);
+    }
+    return tries; //
+}
