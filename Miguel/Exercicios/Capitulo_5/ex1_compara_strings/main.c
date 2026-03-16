226dbb07d671b47e6cf7b0640e9c2a0905743d9e
Miguel-Andrade-Cruz <angelium.eddra1972@gmail.com>
2026-03-16 16:09:17 -0300

    Add cap 5 : ex 1 (untested)

---
+++ Miguel/Exercicios/Capitulo_5/ex1_compara_strings/main.c
@@ -0,0 +1,4 @@
+// 1 - Receba 2 string de ate 10 caracteres via teclado na funcao main(). Faēa uma
+//     funcao para compara-las e retorne como resultado se sćo IGUAIS 1 ou se
+//     DIFERENTES 0 para a funcao main(). Imprima o resultado na funcao main().
+//     (Declare os vetores como variavel global) (nao pode usar funcao de biblioteca)
