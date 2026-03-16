085e88d45e6d2d7d977c19fd060be565e2537a56
Miguel-Andrade-Cruz <angelium.eddra1972@gmail.com>
2026-03-16 16:07:49 -0300

    Add ex 2 (untested)

---
+++ Miguel/Exercicios/Capitulo_4/ex2_vetores_impar_par/main.c
@@ -0,0 +1,5 @@
+// 2 - Escreva um programa que receba um vetor de inteiros com tamanho 10 e o 
+//    decomponha em dois outros vetores. Um tera“ as componentes de ordem impar 
+//    e o outro tera“ as componentes de ordem par.
+//    Por exemplo, se o vetor dado for v = {3, 5, 6, 8, 1, 4, 2, 3, 7, 9}, o programa
+//    deve gerar os vetores u = {3, 6, 1, 2, 7} e w = {5, 8, 4, 3, 9}.
