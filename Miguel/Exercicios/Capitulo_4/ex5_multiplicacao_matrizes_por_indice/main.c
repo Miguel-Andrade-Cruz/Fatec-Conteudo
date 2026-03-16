8246f3be4d6b67702667c54c0444b64ccf2680cb
Miguel-Andrade-Cruz <angelium.eddra1972@gmail.com>
2026-03-16 16:08:16 -0300

    Add ex 5 (untested)

---
+++ Miguel/Exercicios/Capitulo_4/ex5_multiplicacao_matrizes_por_indice/main.c
@@ -0,0 +1,5 @@
+// 5 - Defina 2 vetores bidimensionais do tipo inteiro de 2x3. Escreva um programa
+//    que recebe os dados via teclado para esses 2 vetores. Usando o operador
+//    multiplicacao '*', multiplique os elementos de mesmo indice dos 2 vetores e guarde
+//    o resultado em um 3. vetor. Imprima na tela os indices, os valores e o resultado
+//    dos 6 elementos dos vetores.
