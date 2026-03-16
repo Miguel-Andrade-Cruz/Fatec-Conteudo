498d95098469ebb86ba3ffb352db326071d34fc3
Miguel-Andrade-Cruz <angelium.eddra1972@gmail.com>
2026-03-16 16:09:25 -0300

    Add ex 2 (untested)

---
+++ Miguel/Exercicios/Capitulo_5/ex2_formata_impressao/main.c
@@ -0,0 +1,10 @@
+// 2 - Escreva um programa para receber 5 nomes, com até 7 caracteres, via teclado
+//     e imprima-os no video no seguinte formato:
+//     (Declare os 5 vetores como variaveis globais)
+//
+//              10        20        30        40        50
+//    12345678901234567890123456789012345678901234567890
+//      nome1                                   nome5
+//                nome2               nome4
+//                          nome3
+
