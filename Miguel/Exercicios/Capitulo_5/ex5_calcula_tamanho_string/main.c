8c7e452df8757f1b83b7e59573f7d0354b2ad9ff
Miguel-Andrade-Cruz <angelium.eddra1972@gmail.com>
2026-03-16 16:10:04 -0300

    Add ex 5 (untested)

---
+++ Miguel/Exercicios/Capitulo_5/ex5_calcula_tamanho_string/main.c
@@ -0,0 +1,4 @@
+// 5 - Escreva um programa que receba uma string de comprimento até 40 na funcao
+//     main(). Faca uma funcao que calcula o comprimento da string recebida via teclado.                
+//     Mostre o valor do comprimento na funcao main().
+//     (Declare o vetor como variavel global) (nao pode usar funcao de biblioteca)
