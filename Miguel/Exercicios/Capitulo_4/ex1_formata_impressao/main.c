78b783d6291a55f8e3cde14c6edfe134ca0c9d47
Miguel-Andrade-Cruz <angelium.eddra1972@gmail.com>
2026-03-16 16:07:31 -0300

    Add cap 4 : ex 1 (untested)

---
+++ Miguel/Exercicios/Capitulo_4/ex1_formata_impressao/main.c
@@ -0,0 +1,12 @@
+// 1 - Escreva um programa para receber via teclado em vetores 3 int, 3 long,
+//    3 unsigned, 3 float e 3 double e imprima-os no video no seguinte formato:
+//
+//          10        20        30        40        50
+//    12345678901234567890123456789012345678901234567890
+//      int                 long                unsigned
+//                float               double
+//      int                 long                unsigned
+//                float               double
+//      int                 long                unsigned
+//                float               double
+
