86a6caad32448cead855eaa276f392fb260e9c4c
Miguel-Andrade-Cruz <angelium.eddra1972@gmail.com>
2026-03-17 17:00:50 -0300

    CAP 5 EX 2
    Write boilerplate pattern

--- Miguel/Exercicios/Capitulo_5/ex2_formata_impressao/main.c
+++ Miguel/Exercicios/Capitulo_5/ex2_formata_impressao/main.c
@@ -8,3 +8,18 @@
 //                nome2               nome4
 //                          nome3
 
+#include "middleware.h"
+
+
+int main() {
+
+    char again = 'n';
+    do {
+        
+        // +-+-+-+-+-+-+-+-+-+-+-+-+-+
+        printf("Deseja executar novamente? ( s / n )  ");
+        scanf("%c", &again);
+        clear_buffer();
+    } while (again == 's');
+    return 0;
+}
