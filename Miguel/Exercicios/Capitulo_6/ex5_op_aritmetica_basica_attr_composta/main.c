b62d8e37cee4a48aa4dd8948bf15e9a685285cbb
Miguel-Andrade-Cruz <angelium.eddra1972@gmail.com>
2026-03-17 16:02:14 -0300

    CAP 6 EX 5
    Write boilerplate pattern

--- Miguel/Exercicios/Capitulo_6/ex5_op_aritmetica_basica_attr_composta/main.c
+++ Miguel/Exercicios/Capitulo_6/ex5_op_aritmetica_basica_attr_composta/main.c
@@ -1,2 +1,18 @@
 // 5 - Reescreva o exercicio anterior utilizando operadores de atribuicao
 //    composta.
+
+#include "middleware.h"
+
+
+int main() {
+
+    char again = 'n';
+    do {
+        
+        // +-+-+-+-+-+-+-+-+-+-
+        printf("Deseja executar novamente? ( s / n )  ");
+        scanf("%c", &again);
+        clear_buffer();
+    } while (again == 's');
+    return 0;
+}
