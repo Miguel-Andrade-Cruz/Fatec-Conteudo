84a23324b60a8adf7591309b30716f76475a11e5
Miguel-Andrade-Cruz <angelium.eddra1972@gmail.com>
2026-03-17 16:30:35 -0300

    CAP 5 EX 1
    Write boilerplate pattern

--- Miguel/Exercicios/Capitulo_5/ex1_compara_strings/main.c
+++ Miguel/Exercicios/Capitulo_5/ex1_compara_strings/main.c
@@ -1,4 +1,22 @@
-// 1 - Receba 2 string de ate 10 caracteres via teclado na funcao main(). Faēa uma
-//     funcao para compara-las e retorne como resultado se sćo IGUAIS 1 ou se
+// 1 - Receba 2 string de ate 10 caracteres via teclado na funcao main(). Faça uma
+//     funcao para compara-las e retorne como resultado se são IGUAIS 1 ou se
 //     DIFERENTES 0 para a funcao main(). Imprima o resultado na funcao main().
 //     (Declare os vetores como variavel global) (nao pode usar funcao de biblioteca)
+
+#include "middleware.h"
+
+
+char string_one[10], string_two[10];
+
+int main() {
+
+    char again = 'n';
+    do {
+        
+        // +-+-+-+-+-+-+-+-+-+-+-+-+-+
+        printf("Deseja executar novamente? ( s / n )  ");
+        scanf("%c", &again);
+        clear_buffer();
+    } while (again == 's');
+    return 0;
+}
