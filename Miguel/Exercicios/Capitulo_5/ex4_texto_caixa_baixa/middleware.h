935cac11b353b4e0331a36473d68ce5ed52053b0
Miguel-Andrade-Cruz <angelium.eddra1972@gmail.com>
2026-03-17 23:48:36 -0300

    CAP 5 EX 3
    First exercise implementation (untested)

--- Miguel/Exercicios/Capitulo_5/ex4_texto_caixa_baixa/middleware.h
+++ Miguel/Exercicios/Capitulo_5/ex4_texto_caixa_baixa/middleware.h
@@ -3,3 +3,9 @@
 #define STRING_SIZE 10
 
 void clear_buffer();
+
+void ask_for_string(char *string);
+
+void to_lowercase_conversion(char *string);
+
+void show_string_change(char *string);
--- Miguel/Exercicios/Capitulo_5/ex4_texto_caixa_baixa/midlleware.c
+++ Miguel/Exercicios/Capitulo_5/ex4_texto_caixa_baixa/midlleware.c
@@ -6,3 +6,30 @@ void clear_buffer() {
     while (getchar() != '\n');
     return;
 }
+
+
+void ask_for_string(char *string) {
+    
+    printf("Insira uma string (máximo 10 caracteres):  ");
+    scanf("%s", string);
+    clear_buffer();
+    return;
+}
+
+
+void to_lowercase_conversion(char *string) {
+    
+    for (int i_char; i_char < STRING_SIZE; i_char++) {
+        if (string[i_char] == 0x20) {
+            continue;
+        }
+        string[i_char] += 0x20;
+    }
+    return;
+}
+
+void show_string_change(char *string) {
+    
+    printf("Assim ficou a string em caixa alta: %s", string);
+    return;
+}
