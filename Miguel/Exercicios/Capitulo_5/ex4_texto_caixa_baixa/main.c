3806c57e9add8103178f1e2ef91d00c83e3a8847
Miguel-Andrade-Cruz <angelium.eddra1972@gmail.com>
2026-03-17 23:39:37 -0300

    CAP 5 EX 4
    Write boilerplate pattern

--- Miguel/Exercicios/Capitulo_5/ex4_texto_caixa_baixa/main.c
+++ Miguel/Exercicios/Capitulo_5/ex4_texto_caixa_baixa/main.c
@@ -1,2 +1,18 @@
 // 4 - Receba via teclado uma string de comprimento 10 em letras maiusculas  e converta 
 //     todos os caracteres para letras minusculas. (nao pode usar funcao de biblioteca)
+
+#include "middleware.h"
+
+
+int main() {
+
+    char again = 'n';
+    do {
+        
+        // +-+-+-+-+-+-+-+-+-+-+-+-+-+
+        printf("Deseja executar novamente? ( s / n )  ");
+        scanf("%c", &again);
+        clear_buffer();
+    } while (again == 's');
+    return 0;
+}
---
+++ Miguel/Exercicios/Capitulo_5/ex4_texto_caixa_baixa/middleware.h
@@ -0,0 +1,5 @@
+#include <stdio.h>
+
+#define STRING_SIZE 10
+
+void clear_buffer();
