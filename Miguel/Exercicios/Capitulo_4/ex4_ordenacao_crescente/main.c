c951bf8d13e64d9c0cbc6c2b79619c3a8c64f28a
Miguel-Andrade-Cruz <angelium.eddra1972@gmail.com>
2026-03-23 15:49:49 -0300

    CAP 4 EX 4
    Write boilerplate pattern

--- Miguel/Exercicios/Capitulo_4/ex4_ordenacao_crescente/main.c
+++ Miguel/Exercicios/Capitulo_4/ex4_ordenacao_crescente/main.c
@@ -1,3 +1,22 @@
 // 4 - Receba via teclado 10 valores inteiros e ordene por ordem crescente assim
 //    que sao digitados. Guarde-os em um vetor. Mostre ao final os valores 
 //    ordenados.
+
+#include "middleware.h"
+
+
+int main() {
+    
+    char again = 'n';
+    do {
+        
+        
+        
+        
+        // +-+-+-+-+-+-+-+-+-+-+-+-+-+
+        printf("Deseja executar novamente? ( s / n )  ");
+        scanf("%c", &again);
+        clear_buffer();
+    } while (again == 's');
+    return 0;
+}
