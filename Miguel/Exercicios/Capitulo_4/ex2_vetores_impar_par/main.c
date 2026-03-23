2b05fa318a7bf5861f3cb6b90818b9f215760fb5
Miguel-Andrade-Cruz <angelium.eddra1972@gmail.com>
2026-03-23 14:37:07 -0300

    CAP 4 EX 2
    => Rename file miseplling file
    
    => Transfer core logic into main function
    
    => Hide core logic made by functions with comments

--- Miguel/Exercicios/Capitulo_4/ex2_vetores_impar_par/main.c
+++ Miguel/Exercicios/Capitulo_4/ex2_vetores_impar_par/main.c
@@ -7,20 +7,61 @@
 #include "middleware.h"
 
 
+int input_vec[VEC_SIZE], even_vec[VEC_SIZE / 2], odd_vec[VEC_SIZE / 2];
+
 int main() {
 
     char again = 'n';
     do {
-        int input_vec[VEC_SIZE], even_vec[VEC_SIZE / 2], odd_vec[VEC_SIZE / 2];
-        ask_for_10_numbers(input_vec);
         
-        extract_even_nums(input_vec, even_vec);
-        extract_odd_nums(input_vec, odd_vec);
+        printf("Insira 10 números:  \n");
+        for (int i_num = 0; i_num < VEC_SIZE; i_num++) {
+            
+            printf("%d -> ", i_num + 1);
+            scanf("%d", &input_vec[i_num]);
+            clear_buffer();
+            printf("VETOR INPUT INDICE %d : %d\n", i_num, input_vec[i_num]);//
+        }
         
-        show_vecs(even_vec, odd_vec);
+        // To split the vector into evens and odds
+        for (int i_even = 0, i_odd = 0, i_ipt = 0; i_ipt < VEC_SIZE; i_ipt++) {
+            
+            if (input_vec[i_ipt] % 2 == 0) {
+                
+                even_vec[i_even] = input_vec[i_ipt];
+                i_even++;
+            } else {
+                
+                odd_vec[i_odd] = input_vec[i_ipt];
+                i_odd++;
+            }
+        }
+        
+        // Showing the numbers
+        // Odds
+        printf("Números pares passados: ");
+        printf("{  ");
+        for (int i_num = 0; i_num < VEC_SIZE / 2; i_num++) {
+            
+            printf("%d  ", even_vec[i_num]);
+        }
+        printf("}");
+        
+        
+        printf("\n\n");
+        
+        
+        // Evens
+        printf("Números ímpares passados: ");
+        printf("{  ");
+        for (int i_num = 0; i_num < VEC_SIZE / 2; i_num++) {
+            
+            printf("%d  ", odd_vec[i_num]);
+        }
+        printf("}");
         
         // +-+-+-+-+-+-+-+-+-+-+-+-+-+
-        printf("Deseja executar novamente? ( s / n )  ");
+        printf("\nDeseja executar novamente? ( s / n )  ");
         scanf("%c", &again);
         clear_buffer();
     } while (again == 's');
--- Miguel/Exercicios/Capitulo_4/ex2_vetores_impar_par/middleware.h
+++ Miguel/Exercicios/Capitulo_4/ex2_vetores_impar_par/middleware.h
@@ -4,10 +4,10 @@
 
 void clear_buffer();
 
-void ask_for_10_numbers(int *input_vec);
+// void ask_for_10_numbers(int *input_vec);
 
-void extract_even_nums(int *input, int *returned);
+// void extract_even_nums(int *input, int *returned);
 
-void extract_odd_nums(int *input, int *returned);
+// void extract_odd_nums(int *input, int *returned);
 
-void show_vecs(int *even_vec, int *odd_vec);
+// void show_vecs(int *even_vec, int *odd_vec);
