79f3e357760fdebaaf9002f6c533cb7cbac8f1ea
Miguel-Andrade-Cruz <angelium.eddra1972@gmail.com>
2026-03-25 16:39:16 -0300

    CAP 4 EX 5
    => Write boilerplate pattern

--- Miguel/Exercicios/Capitulo_4/ex5_multiplicacao_matrizes_por_indice/main.c
+++ Miguel/Exercicios/Capitulo_4/ex5_multiplicacao_matrizes_por_indice/main.c
@@ -3,3 +3,22 @@
 //    multiplicacao '*', multiplique os elementos de mesmo indice dos 2 vetores e guarde
 //    o resultado em um 3. vetor. Imprima na tela os indices, os valores e o resultado
 //    dos 6 elementos dos vetores.
+
+#include "middleware.h"
+
+
+int main() {
+    
+    char again = 'n';
+    do {
+        
+        
+        
+        
+        // +-+-+-+-+-+-+-+-+-+-+-+-+
+        printf("Desja executar novamente? ( s / n )  ");
+        scanf("%c", &again);
+        clear_buffer();
+    } while (again == 's');
+    return 0;
+}
