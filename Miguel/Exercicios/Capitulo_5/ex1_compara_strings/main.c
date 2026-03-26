fd38a271d69f6d61208dd3968a16e682c8eea778
Miguel-Andrade-Cruz <angelium.eddra1972@gmail.com>
2026-03-26 10:20:32 -0300

    CAP 5 EX 1
    => Rename file from 'midlleware' to 'middleware'
    
    => Implement core logic, with INNER (all inside main) and OUTER (with
    functions) mode
    
    => Add DEBUG mode for error checking

--- Miguel/Exercicios/Capitulo_5/ex1_compara_strings/main.c
+++ Miguel/Exercicios/Capitulo_5/ex1_compara_strings/main.c
@@ -4,10 +4,14 @@
 //     (Declare os vetores como variavel global) (nao pode usar funcao de biblioteca)
 
 #include "middleware.h"
+// #define DEBUG
+#define INNER_MODE
+// #define OUTER_MODE
 
+char string_one[STRING_SIZE + 1];
+char string_two[STRING_SIZE + 1];
 
-char string_one[STRING_SIZE + 1], string_two[STRING_SIZE + 1];
-
+#ifdef OUTER_MODE
 int main() {
 
     char again = 'n';
@@ -23,5 +27,60 @@ int main() {
         scanf("%c", &again);
         clear_buffer();
     } while (again == 's');
+    
     return 0;
 }
+#endif
+
+
+#ifdef INNER_MODE
+int main() {
+
+    char again = 'n';
+    do {
+        
+        // 'ask_for_two_strings' function
+        printf("Insira a primeira string (máximo 10 carcteres):  ");
+        scanf("%s", string_one);
+        clear_buffer();
+        
+        printf("Insira a segunda string (máximo 10 carcteres):  ");
+        scanf("%s", string_two);
+        clear_buffer();
+        
+        #ifdef DEBUG
+        printf("[DEBUG] FIRST STRING: %s\n", string_one);
+        printf("[DEBUG] SECOND STRING: %s\n", string_two);
+        #endif
+        // -----------------------------------------
+        
+        
+        // 'string_comparator' function
+        int equal_chars_qtd = 0;
+        for (int i_char = 0; i_char < STRING_SIZE; i_char++) {
+            
+            if (string_one[i_char] == string_two[i_char]) {
+                equal_chars_qtd++;
+            }
+        }
+        int is_equal = equal_chars_qtd == STRING_SIZE;
+        printf("[DEBUG] IS_EQUAL: %d\n", is_equal);
+        // -----------------------------------------
+        
+        
+        // 'show_comparison_result' function
+        if (is_equal == 1) {
+        printf("As duas strings são iguais\n");
+        } else {
+            printf("Há diferenças entre as strings\n");
+        }
+        // -----------------------------------------
+        
+        // +-+-+-+-+-+-+-+-+-+-+-+-+-+
+        printf("Deseja executar novamente? ( s / n )  ");
+        scanf("%c", &again);
+        clear_buffer();
+    } while (again == 's');
+    return 0;
+}
+#endif
--- Miguel/Exercicios/Capitulo_5/ex1_compara_strings/middleware.c
+++ Miguel/Exercicios/Capitulo_5/ex1_compara_strings/middleware.c
@@ -1,4 +1,5 @@
 #include "middleware.h"
+// #define DEBUG
 
 
 void clear_buffer() {
@@ -10,27 +11,38 @@ void clear_buffer() {
 
 void ask_for_two_strings(char *str_one, char * str_two) {
     
-    printf("Insira a primeira string (máximo 9 carcteres):  ");
+    printf("Insira a primeira string (máximo 10 carcteres):  ");
     scanf("%s", str_one);
     clear_buffer();
     
-    printf("Insira a segunda string (máximo 9 carcteres):  ");
+    printf("Insira a segunda string (máximo 10 carcteres):  ");
     scanf("%s", str_two);
     clear_buffer();
+    
+    #ifdef DEBUG
+    printf("[DEBUG] FIRST STRING: %s < ask_for_two_strings > -[ ln 12 ]-\n", str_one);
+    printf("[DEBUG] SECOND STRING: %s < ask_for_two_strings > -[ ln 12 ]-\n", str_two);
+    #endif
     return;
 }
 
 
 int string_comparator(char *str_one, char * str_two) {
     
+    int is_equal;
     int equal_chars_qtd = 0;
     for (int i_char = 0; i_char < STRING_SIZE; i_char++) {
         
-        if (str_one[i_char] /= str_two[i_char]) {
+        if (str_one[i_char] == str_two[i_char]) {
             equal_chars_qtd++;
         }
     }
-    return equal_chars_qtd == STRING_SIZE;
+    is_equal = equal_chars_qtd == STRING_SIZE;
+    
+    #ifdef DEBUG
+    printf("[DEBUG] IS_EQUAL: %d < string_comparator > -[ ln 30 ]-\n", is_equal);
+    #endif
+    return is_equal
 }
 
 
