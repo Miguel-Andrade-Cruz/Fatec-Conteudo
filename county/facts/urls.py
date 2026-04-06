64f1929aff13b652ea82e97b7404e3f625527773
Miguel-Andrade-Cruz <angelium.eddra1972@gmail.com>
2026-04-06 13:46:23 -0300

    SISTEMA
    => Cria app para fatos (em desenvolvimento)

--- county/facts/urls.py
+++ county/facts/urls.py
@@ -5,6 +5,6 @@ from . import views
 app_name = "facts"
 
 urlpatterns = [
-    path('', views.home, name="home"),
-    path('/novo', views.form, name="home")
+    # path('', views.home, name="home"),
+    # path('/novo', views.form, name="home")
 ]
--- county/facts/views.py
+++ county/facts/views.py
@@ -1,26 +1,26 @@
 from django.shortcuts import render
 
-from .middleware import format_acc_chart
-from .models import Facts
+# from .middleware import format_acc_chart
+# from .models import Facts
 
 
-def home(request):
+# def home(request):
 
-    qs_records = Facts.objects.all().values()
-    records = format_acc_chart(qs_records)
+#     qs_records = Facts.objects.all().values()
+#     records = format_acc_chart(qs_records)
 
-    return render(request, "home.html", context={"facts": records})
+#     return render(request, "home.html", context={"facts": records})
 
 
-def form(request):
+# def form(request):
 
-    if request.method == "POST":
+#     if request.method == "POST":
 
-        form = FactForm(request.POST)
-        if form.is_valid():
-            data = form.cleaned_data
+#         form = FactForm(request.POST)
+#         if form.is_valid():
+#             data = form.cleaned_data
 
-    else:
-        form = FactForm()
+#     else:
+#         form = FactForm()
 
-    return render(request, "form.html", {"form": form})
+#     return render(request, "form.html", {"form": form})
