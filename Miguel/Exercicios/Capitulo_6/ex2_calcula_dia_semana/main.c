01894255a22153cd60629490cd93d5d9b063cc81
Miguel-Andrade-Cruz <angelium.eddra1972@gmail.com>
2026-04-08 17:23:49 -0300

    CAP 6 EX 2
    => Adapt main function flow for the new logic
    
    => Add INNER and OUTER mode
    
    => Write boilerplate code for INNER_MODE

--- Miguel/Exercicios/Capitulo_6/ex2_calcula_dia_semana/main.c
+++ Miguel/Exercicios/Capitulo_6/ex2_calcula_dia_semana/main.c
@@ -1,24 +1,24 @@
 // 2 - Escreva um programa que receba via teclado uma data (dia, mes, e ano).
 //     Determine o dia da semana desta data.
 
+// #define OUTER_MODE
+#define INNER_MODE
 
+#ifdef OUTER_MODE
 #include "middleware.h"
-
-
 int main() {
 
     char again = 'n';
     do {
+        
+        Date date_input;
+        ask_date(&date_input);
+        
         char weekday[8];
-        int day, month, year;
-        ask_date(&day, &month, &year);
+        resolve_weekday(date_input, weekday);
         
-        int is_leap_year = leap_year_check(year);
-        int total_leap_years = calculate_total_leap_years(year);
-        int total_days = get_days_since_start_point(day, month, year, is_leap_year, total_leap_years);
+        show_weekday_date(date_input, weekday);
         
-        resolve_weekday(total_days, weekday);
-        show_weekday_date(day, month, year, weekday);
         // +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
         printf("Deseja executar novamente? (s / n)");
         scanf("%c", &again);
@@ -26,3 +26,41 @@ int main() {
     } while (again == 's');
     return 0;
 }
+#endif // OUTER_MODE
+
+
+
+#ifdef INNER_MODE
+#include <stdio.h>
+
+void clear_buffer() {
+    
+    while (getchar() != '\n');
+    return;
+}
+
+const char DAYS_OF_THE_WEEK[7][8] = {
+    "Domingo",
+    "Segunda",
+    "Terça",
+    "Quarta",
+    "Quinta",
+    "Sexta",
+    "Sábado"
+};
+
+
+int main() {
+    
+    
+    char again = 'n';
+    do {
+        
+        // +-+-+-+-+-+-+-+-+-+-+
+        printf("Deseja executar novamente ( s / n )  ");
+        scanf("%c", &again);
+        clear_buffer();
+    } while (getchar() != '\n');
+    return 0;
+}
+#endif // INNER_MODE
