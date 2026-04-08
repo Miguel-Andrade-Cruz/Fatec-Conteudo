f3e4d29379a3b583afaf467970c68cd18c832718
Miguel-Andrade-Cruz <angelium.eddra1972@gmail.com>
2026-04-08 16:14:02 -0300

    CAP 6 EX 3
    => Implement algorithm for INNER_MODE

--- Miguel/Exercicios/Capitulo_6/ex3_operacoes_bitwise/main.c
+++ Miguel/Exercicios/Capitulo_6/ex3_operacoes_bitwise/main.c
@@ -45,7 +45,33 @@ int main() {
     char again = 's';
     do {
         
+        // user input ------ <
+        int num1, num2;
+        printf("Insira o primeiro número:  ");
+        scanf("%d", &num1);
+        clear_buffer();
         
+        printf("Insira o segundo número:  ");
+        scanf("%d", &num2);
+        clear_buffer();
+        // ------------ >
+        
+        // bitwise operations ------ <
+        int bitwise_and = num1 & num2;
+        printf("Expressão: %d AND %d = %d(10) ou %x(16)\n\n",
+            num1, num2, bitwise_and, bitwise_and
+        );
+        
+        int bitwise_or = num1 | num2;
+        printf("Expressão: %d OR %d = %d(10) ou %x(16)\n\n",
+            num1, num2, bitwise_or, bitwise_or
+        );
+        
+        int bitwise_xor = num1 ^ num2;
+        printf("Expressão: %d XOR %d = %d(10) ou %x(16)\n\n",
+            num1, num2, bitwise_xor, bitwise_xor
+        );
+        // ------------ >
         
         // +-+-+-+-+-+-+-+-+-+-+-+-+-+
         printf("Deseja executar novamente ( s / n )  ");
