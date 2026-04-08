b4daaed4d9031877b61d3242127baaecb202c555
Miguel-Andrade-Cruz <angelium.eddra1972@gmail.com>
2026-04-08 17:59:05 -0300

    CAP 6 EX 2
    => Implement INNER_MODE algorithm

--- Miguel/Exercicios/Capitulo_6/ex2_calcula_dia_semana/main.c
+++ Miguel/Exercicios/Capitulo_6/ex2_calcula_dia_semana/main.c
@@ -49,6 +49,14 @@ const char DAYS_OF_THE_WEEK[7][8] = {
     "Sábado"
 };
 
+const int month_offset[] = {
+    0, 3, 2, 
+    5, 0, 3, 
+    5, 1, 4, 
+    6, 2, 4
+};
+
+const char article[4] = "uma";
 
 int main() {
     
@@ -56,11 +64,42 @@ int main() {
     char again = 'n';
     do {
         
+        int day, month, year;
+        printf("Insira a data que deseja no formato 00/00/0000  :");
+        scanf("%d/%d/%d", &day, &month, &year);
+        clear_buffer();
+        
+        int v_year = year;
+        
+        char weekday[8];
+        if ( month < 3) {
+            v_year = year - 1;
+        }
+        int leap_years = (v_year / 4) - (v_year / 100) + (v_year / 400);
+        
+        // 'dow' means 'day of week'
+        int dow_index = (year + leap_years + month_offset[month - 1] + day) % 7;
+        
+        
+        for (int i = 0; i < 8; i++) {
+            weekday[i] = DAYS_OF_THE_WEEK[dow_index][i];
+        }
+        
+        int article_limit = 2;
+        if (!(dow_index == 0) && !(dow_index == 6)) {
+            article_limit++;
+        }
+        
+        printf(
+            "O dia %.2d/%.2d/%.4d foi %.*s %s\n",
+            day, month, year, article_limit, article, weekday
+        );
+        
         // +-+-+-+-+-+-+-+-+-+-+
         printf("Deseja executar novamente ( s / n )  ");
         scanf("%c", &again);
         clear_buffer();
-    } while (getchar() != '\n');
+    } while (getchar() != 's');
     return 0;
 }
 #endif // INNER_MODE
