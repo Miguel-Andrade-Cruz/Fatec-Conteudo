aff53691692112d2c2cc3d1ecbffdc0c5304b4f4
Miguel-Andrade-Cruz <angelium.eddra1972@gmail.com>
2026-04-07 23:01:20 -0300

    CAP 6 EX 5
    => Implement algorithm on INNER mode
    
    => Reallocate include directives

--- Miguel/Exercicios/Capitulo_6/ex5_op_aritmetica_basica_attr_composta/main.c
+++ Miguel/Exercicios/Capitulo_6/ex5_op_aritmetica_basica_attr_composta/main.c
@@ -1,11 +1,11 @@
 // 5 - Reescreva o exercicio anterior utilizando operadores de atribuicao
 //    composta.
 
-#include "middleware.h"
-// #define INNER_MODE
-#define OUTER_MODE
+#define INNER_MODE
+// #define OUTER_MODE
 
-#ifdef INNER_MODE
+#ifdef OUTER_MODE
+#include "middleware.h"
 int main() {
     
     char again = 'n';
@@ -26,18 +26,52 @@ int main() {
     } while (again == 's');
     return 0;
 }
-#endif // INNER_MODE
+#endif // OUTER_MODE
 
 
 
 
-#ifdef OUTER_MODE
+
+
+#ifdef INNER_MODE
+#include <stdio.h>
+void clear_buffer() {
+    
+    while (getchar() != '\n');
+    return;
+}
+
+
 int main() {
     
     char again = 'n';
     do {
         
+        float num1, num2;
+        printf("Digite o primeiro número:  ");
+        scanf("%f", &num1);
         
+        printf("Digite o segundo número:  ");
+        scanf("%f", &num2);
+        
+        float
+            acc_sum = num1 ,
+            acc_sub = num1 ,
+            acc_mult = num1 ,
+            acc_div = num1
+        ;
+        
+        acc_sum += num2;
+        printf("A soma entre %.2f e %.2f é igual a %.2f\n\n", num1, num2, acc_sum);
+        
+        acc_sub -= num2;
+        printf("A subtração entre %.2f e %.2f é igual a %.2f\n\n", num1, num2, acc_sub);
+        
+        acc_mult *= num2;
+        printf("A multiplicação entre %.2f e %.2f é igual a %.2f\n\n", num1, num2, acc_mult);
+        
+        acc_div /= num2;
+        printf("A divisão entre %.2f e %.2f é igual a %.2f\n\n", num1, num2, acc_div);
         
         
         // +-+-+-+-+-+-+-+-+-+-+-+
@@ -49,5 +83,4 @@ int main() {
     
     return 0;
 }
-
-#endif // OUTER_MODE
+#endif // INNER_MODE
