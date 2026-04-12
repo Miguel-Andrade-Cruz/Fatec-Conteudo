621142c0b34f3b755962a2273c8bbfc6ff026a36
Miguel-Andrade-Cruz <angelium.eddra1972@gmail.com>
2026-04-12 15:49:34 -0300

    CAP  1 EX 5
    => Implement INNER mode of the algorithm

--- Miguel/Exercicios/Capitulo_1/ex4_menor_multiplo/main.c
+++ Miguel/Exercicios/Capitulo_1/ex4_menor_multiplo/main.c
@@ -3,9 +3,16 @@
 //      teclado.
 //     Ex: menor multiplo de 13 maior que 100. Resultado: 104.
 
-#include <stdio.h>
-#include "middleware.h"
+#define INNER_MODE
+// #define OUTER_MODE
 
+
+
+
+
+
+#ifdef OUTER_MODE
+#include "middleware.h"
 
 int main() {
     
@@ -24,3 +31,78 @@ int main() {
     } while (again == 's');
     return 0;
 }
+#endif // OUTER_MODE
+
+
+
+#ifdef INNER_MODE
+#include <stdio.h>
+
+void clear_buffer() {
+    
+    while (getchar() != '\n');
+    return;
+}
+
+int input_reference_number() {
+    
+    int number;
+    printf("Digite seu número de referência (número positivo): ");
+    do {
+        scanf("%d", &number);
+        clear_buffer();
+    } while (number <= 0);
+    
+    return number;
+}
+
+int input_minimal_limit() {
+    
+    int number;
+    printf("Digite o limite (número positivo): ");
+    do {
+        scanf("%d", &number);
+        clear_buffer();
+    } while (number <= 0);
+    
+    return number;
+}
+
+int get_minimal_factor(int reference_number, int minimal_limit) {
+    
+    int minimal_factor;
+    
+    if (minimal_limit <= reference_number) {
+        minimal_factor = reference_number;
+    } else {
+        
+        int aprox_minimal_factor = minimal_factor / reference_number - 1;
+        
+        int next_factor = reference_number * aprox_minimal_factor;
+        while ( next_factor < minimal_limit ) {
+            next_factor += reference_number;
+        }
+        minimal_factor = next_factor;
+    }
+
+    return minimal_factor;
+}
+
+int main() {
+    
+    char again = 'n';
+    do {
+        int reference_number = input_reference_number();
+        int minimal_limit = input_minimal_limit();
+        int minimal_factor = get_minimal_factor(reference_number, minimal_limit);
+        
+        printf("O menor múltiplo de %d maior que %d é %d\n", reference_number, minimal_limit, minimal_factor);
+        
+        // +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+        printf("Deseja executar novamente? ( s / n)  ");
+        scanf("%c", &again);
+        clear_buffer();
+    } while (again == 's');
+    return 0;
+}
+#endif // INNER_MODE
