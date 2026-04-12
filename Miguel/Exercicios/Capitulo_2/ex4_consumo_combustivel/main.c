b685721112e2265dd16b5d116701049af31c2068
Miguel-Andrade-Cruz <angelium.eddra1972@gmail.com>
2026-04-12 14:22:57 -0300

    CAP 2 EX 4
    => Implement INNER mode of the algorithm

--- Miguel/Exercicios/Capitulo_2/ex4_consumo_combustivel/main.c
+++ Miguel/Exercicios/Capitulo_2/ex4_consumo_combustivel/main.c
@@ -7,8 +7,12 @@
 //    entre 8 e 14   Economico!
 //    maior que 14   Super economico
 
-#include "middleware.h"
+#define INNER_MODE
+// #define OUTER_MODE
 
+
+#ifdef OUTER_MODE
+#include "middleware.h"
 
 int main() {
     
@@ -29,3 +33,83 @@ int main() {
     
     return 0;
 }
+#endif // OUTER_MODE
+
+
+#ifdef INNER_MODE
+#include <stdio.h>
+
+void clear_buffer() {
+    
+    while (getchar() != '\n');
+    return;
+}
+
+
+float ask_kilometers_traveled() {
+    
+    float kilometers_traveled;
+    
+    printf("Digite a distância percorrida em quilômetros:  ");
+    scanf("%f", &kilometers_traveled);
+    clear_buffer();
+    return kilometers_traveled;
+}
+
+
+float ask_gasoline_consumed() {
+    
+    float gasoline_consumed;
+    
+    printf("Digite a  consumo de gasolina em litros:  ");
+    scanf("%f", &gasoline_consumed);
+    clear_buffer();
+    return gasoline_consumed;
+}
+
+
+float calculate_consumption(float kilometers_traveled, float gasoline_consumed) {
+    
+    float km_per_liter = kilometers_traveled / gasoline_consumed;
+    return km_per_liter;
+}
+
+
+void show_consumption(float consumption) {
+    
+    const int LOW_EFFICIENCY = 8;
+    const int HIGH_EFFICIENCY = 14;
+    
+    printf("Consumo médio: %.1fKm/L  ", consumption);
+    if (consumption < LOW_EFFICIENCY) {
+        printf("Venda o carro!\n");
+        
+    } else if (consumption > HIGH_EFFICIENCY) {
+        printf("Super econômico\n");
+        
+    } else {
+        printf("Econômico!\n");
+    }
+    return;
+}
+
+int main() {
+    
+    char again = 'n';
+    do {
+        
+        float kilometers_traveled = ask_kilometers_traveled();
+        float gasoline_liters = ask_gasoline_consumed();
+        
+        float average_consumption = calculate_consumption(kilometers_traveled, gasoline_liters);
+        show_consumption(average_consumption);
+        
+        // +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+        printf("Deseja executar novamente? ( s / n )");
+        scanf("%c", &again);
+        clear_buffer();
+    } while (again == 's');
+    
+    return 0;
+}
+#endif // INNER_MODE
