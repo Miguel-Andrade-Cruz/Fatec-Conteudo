89c724a6c610cc7bab000d29dc9d8ac97ea33e6e
Miguel-Andrade-Cruz <angelium.eddra1972@gmail.com>
2026-04-13 17:25:53 -0300

    CAP 1 EX 3
    => Create INNER mode of the algorithm (remember to make the OUTER mode
    later)

--- Miguel/Exercicios/Capitulo_1/ex3_formatacao/main.c
+++ Miguel/Exercicios/Capitulo_1/ex3_formatacao/main.c
@@ -1,89 +1,3 @@
-#include <stdio.h>
-
-
-// TODO: Precisa consertar isso aqui ainda
-
-void input_all_types(
-    short short_number,
-    long long_number,
-    int default_number,
-    float float_number,
-    double double_number,
-    char character
-) {
-
-    printf("Insira aqui um short: :\n");
-    scanf("%hd", &short_number);
-    printf("Insira aqui um long: ");
-    scanf("%ld", &long_number);
-    printf("Insira aqui um inteiro: ");
-    scanf("%d", &default_number);
-    printf("Insira aqui um float: ");
-    scanf("%f", &float_number);
-    printf("Insira aqui um double: ");
-    scanf("%lf", &double_number);
-    printf("Insira aqui um char: ");
-    scanf("%c", &character);
-    
-    return;
-}
-
-
-int main() {
-
-    short short_number;
-    long long_number;
-    int default_number;
-    float float_number;
-    double double_number;
-    char character;
-
-    input_all_types(
-        short_number,
-        long_number,
-        default_number,
-        float_number,
-        double_number,
-        character
-    );
-
-    printf("\n");
-    for (int i = 0, step_10 = 10; i < 6; i++, step_10 += 10) {
-
-        printf("%8d", step_10);
-    }
-
-    printf("\n");
-
-    for (int i = 0; i < 5; i++) {
-
-        int one_to_zero = 1234567890;
-        printf("%3d", one_to_zero);
-        
-    }
-    printf("%d", 12345);
-    
-    printf("\n");
-
-
-    printf("%10hd", short_number);
-    printf("%16ld", long_number);
-    printf("%16d", default_number);
-
-    printf("\n");
-
-    printf("%20f", float_number);
-    printf("%15lf", double_number);
-    printf("%14c", character);
-
-    printf("\n");
-
-    return 0;
-}
-
-
-
-
 // 3 - Escreva um programa que receba dados via teclado para variaveis do tipo short,
 //      long, int, float, double e um char e imprima-os no video no seguinte
 //      formato:
@@ -92,3 +6,110 @@ int main() {
 //      12345678901234567890123456789012345678901234567890123456789012345
 //          short               long                int
 //                    float               double              char
+
+#define INNER_MODE
+
+#ifdef INNER_MODE
+#include <stdio.h>
+
+
+void clear_buffer() {
+    
+    while (getchar() != '\n');
+    return;
+}
+
+short ask_short() {
+    
+    short num_short;
+    printf("Número tipo short:  ");
+    scanf("%hd", &num_short);
+    clear_buffer();
+    return num_short;
+}
+
+float ask_float() {
+    
+    float num_float;
+    printf("Número float:   ");
+    scanf("%f", &num_float);
+    clear_buffer();
+    return num_float;
+}
+
+long ask_long() {
+    long num_long;
+    printf("Número long:   ");
+    scanf("%ld", &num_long);
+    clear_buffer();
+    return num_long;
+}
+
+double ask_double() {
+    double num_double;
+    printf("Número double:   ");
+    scanf("%lf", &num_double);
+    clear_buffer();
+    return num_double;
+}
+
+int ask_int() {
+    int num_int;
+    printf("Número int:   ");
+    scanf("%d", &num_int);
+    clear_buffer();
+    return num_int;
+}
+
+char ask_char() {
+    char value_char;
+    printf("Valor char:   ");
+    scanf("%c", &value_char);
+    clear_buffer();
+    return value_char;
+}
+
+
+
+
+int main() {
+    
+    char again = 'n';
+    do {
+        
+        int one_to_five = 12345;
+        int six_to_zero = 67890;
+        
+        short short_number = ask_short();
+        long long_number = ask_long();
+        int int_number = ask_int();
+        float float_number = ask_float();
+        double double_number = ask_double();
+        char character = ask_char();
+        
+        printf("%14d%8d%8d%8d%8d%8d\n",
+            10, 20, 30, 40, 50, 60
+        );
+        printf("%8d%d%d%d%d%d%d%d%d%d%d%d%d\n",
+            one_to_five, six_to_zero, one_to_five,
+            six_to_zero, one_to_five, six_to_zero,
+            one_to_five, six_to_zero, one_to_five,
+            six_to_zero, one_to_five, six_to_zero,
+            one_to_five
+        );
+        
+        printf("%10hi%15li%16d",
+            short_number, long_number, int_number
+        );
+        printf("%15f%20lf%14c",
+            float_number, double_number, character
+        );
+        
+        // +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+        printf("\nDeseja executar novamente ( s / n )  ");
+        scanf("%c", &again);
+        clear_buffer();
+    } while (again == 's');
+    return 0;
+}
+#endif // INNER_MODE
