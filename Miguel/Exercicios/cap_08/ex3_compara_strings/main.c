faac3a83c1fbc14404736d77a8e35e4840f718bd
miguel-webplace <minuz.code@gmail.com>
2026-04-27 10:49:34 -0300

    CAP 8 EX 3
    => Implement resolution for INNER mode ( not tested )

--- Miguel/Exercicios/cap_08/ex3_compara_strings/main.c
+++ Miguel/Exercicios/cap_08/ex3_compara_strings/main.c
@@ -4,11 +4,52 @@
 //     sao IGUAIS 1 ou se sao DIFERENTES 0 para a funcao main(). 
 //     Imprima o resultado na funcao main(). Declare os vetores como variavel global
 
+
+#define INNER_MODE
+#define OUTER_MODE
+
+
+#ifdef INNER_MODE
+
+#include <stdio.h>
+
+void clear_buffer() {
+    
+    while (getchar() == '\n');
+    return;
+}
+
+int strequal(char *str1, char *str2) {
+    
+    int is_equal = 1;
+    while (
+        str1[i] == str2[i] ||
+        str1[i] == '\n' ||
+        str2[i] == '\n'
+    ) {
+        i++;
+    }
+    if ( str1[i] == '\n' && str2[i] == '\n' ) {
+        return 1;
+    }
+    return 0;
+}
+
+
+char *str1, *str2;
+
 int main() {
     
     char again = 'n';
     do {
         
+        printf("Insira a primeira string:  ");
+        scanf("%s", str1);
+        clear_buffer();
+        
+        printf("Insira a segunda string:  ");
+        scanf("%s", str2);
+        clear_buffer();
         
         // +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-
         printf("Deseja executar novamente ( s / n )  ");
@@ -17,3 +58,21 @@ int main() {
     } while (again == 's');
     return 0;
 }
+#ifdef // INNER_MODE
+
+
+#ifdef OUTER_MODE
+int main() {
+    
+    char again = 'n';
+    do {
+
+        // +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-
+        printf("Deseja executar novamente ( s / n )  ");
+        scanf("%c", &again);
+        clear_buffer();
+    } while (again == 's');
+    return 0;
+}
+
+#endif // OUTER_MODE
