879cdd309022072ac121d7ba154bbd1f87c3dd22
miguel-webplace <minuz.code@gmail.com>
2026-04-27 15:54:53 -0300

    CAP 8 EX 2
    => Implement resolution for INNER mode

--- Miguel/Exercicios/cap_08/ex2_media_positivos/main.c
+++ Miguel/Exercicios/cap_08/ex2_media_positivos/main.c
@@ -5,7 +5,65 @@
 //     feitos usando ponteiro.
 // OBS.: As variaveis sao locais na funcao main().
 
+
+#define INNER_MODE
+// #define OUTER_MODE
+
+#ifdef INNER_MODE
+
+#include <stdio.h>
+
+void clear_buffer() {
+    
+    while (getchar() != '\n');
+    return;
+}
+
+
 int main() {
+    
+    char again = 'n';
+    do {
+        
+        int num, *pNum = &num;
+        int count = 0, *pCount = &count;
+        int sum = 0, *pSum = &sum;
+        
+        printf("-->>  ");
+        scanf("%d", pNum);
+        clear_buffer();
+        
+        int done = 0;
+        while (*pNum >= 0) {
+            
+            *pSum += *pNum;
+            *pCount = *pCount + 1;
+            
+            printf("-->>  ");
+            scanf("%d", pNum);
+            clear_buffer();
+        }
+        
+        float median, *pMedian = &median;
+        
+        *pMedian = (float) *pSum / *pCount;
+        printf("A média dos valores é igual a %f\n", *pMedian);
+        
+        // +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-
+        printf("Deseja executar novamente ( s / n )  ");
+        scanf("%c", &again);
+        clear_buffer();
+    } while (again == 's');
+    return 0;
+}
+#endif // INNER_MODE
+
+
+
+#ifdef OUTER_MODE
+
+int main() {
+    
     
     char again = 'n';
     do {
@@ -18,3 +76,4 @@ int main() {
     } while (again == 's');
     return 0;
 }
+#endif // OUTER_MODE
