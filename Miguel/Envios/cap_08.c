ca7778bbb8c85653986cabd45e3ec157f66365d1
miguel-webplace <minuz.code@gmail.com>
2026-04-27 23:29:08 -0300

    EXERCISE
    => Add ex4 to chapter

--- Miguel/Envios/cap_08.c
+++ Miguel/Envios/cap_08.c
@@ -124,6 +124,60 @@ int main() {
 
 // --------------------------------
 #ifdef EX_4
+#include <stdio.h>
+
+
+void clear_buffer() {
+    
+    while (getchar() != '\n');
+    return;
+}
+
+char list[] = {
+    'b','d','f',
+    'h','j','k',
+    'm','o','q',
+    's','u','w',
+    'y'
+};
+
+int search_for_char( char *ipt) {
+    
+    int i = 0;
+    while ( i < 12 ) {
+        
+        if ( list[i] == *ipt ) {
+            return 1;
+        }
+        i++;
+    }
+    
+    return 0; //
+}
+
+
+int main() {
+    
+    char again = 'n';
+    do {
+        
+        char ipt, *pIpt;
+        printf("Digite um caractere:  ");
+        scanf("%c", pIpt);
+        clear_buffer();
+        
+        int exists = search_for_char(pIpt);
+        if (exists) {
+            printf("Esse caractere existe na lista\n");
+        } else {
+            printf("Esse caractere não está presente na lista\n");
+        }
+        // +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-
+        printf("Deseja executar novamente ( s / n )  ");
+        scanf("%c", &again);
+        clear_buffer();
+    } while (again == 's');
+    return 0;
 #endif // EX_4
 // --------------------------------
 
