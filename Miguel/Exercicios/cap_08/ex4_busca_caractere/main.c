1b1fae5632f086aca3dafb7b9115c54d72600242
miguel-webplace <minuz.code@gmail.com>
2026-04-27 23:28:44 -0300

    CAP 8 EX 4
    => Implement resolution for INNER mode

--- Miguel/Exercicios/cap_08/ex4_busca_caractere/main.c
+++ Miguel/Exercicios/cap_08/ex4_busca_caractere/main.c
@@ -7,6 +7,72 @@
 
 //     vetor -> b,d,f,h,j,k,m,o,q,s,u,w,y
 
+#define INNER_MODE
+// #define OUTER_MODE
+
+#ifdef INNER_MODE
+
+#include <stdio.h>
+
+
+void clear_buffer() {
+    
+    while (getchar() != '\n');
+    return;
+}
+
+char list[] = {
+    'b','d','f',
+    'h','j','k',
+    'm','o','q',
+    's','u','w',
+    'y'
+};
+
+int search_for_char( char *ipt) {
+    
+    int i = 0;
+    while ( i < 12 ) {
+        
+        if ( list[i] == *ipt ) {
+            return 1;
+        }
+        i++;
+    }
+    
+    return 0; //
+}
+
+
+int main() {
+    
+    char again = 'n';
+    do {
+        
+        char ipt, *pIpt;
+        printf("Digite um caractere:  ");
+        scanf("%c", pIpt);
+        clear_buffer();
+        
+        int exists = search_for_char(pIpt);
+        if (exists) {
+            printf("Esse caractere existe na lista\n");
+        } else {
+            printf("Esse caractere não está presente na lista\n");
+        }
+        // +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-
+        printf("Deseja executar novamente ( s / n )  ");
+        scanf("%c", &again);
+        clear_buffer();
+    } while (again == 's');
+    return 0;
+}
+#endif // INNER_MODE
+
+
+
+
+#ifdef OUTER_MODE
 int main() {
     
     char again = 'n';
@@ -20,3 +86,4 @@ int main() {
     } while (again == 's');
     return 0;
 }
+#endif // OUTER_MODE
