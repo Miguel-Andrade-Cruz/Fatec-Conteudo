31867bdc52104d6cc467257276de1a62f5f81b35
miguel-webplace <minuz.code@gmail.com>
2026-04-28 22:28:38 -0300

    CAP 9 EX 5
    => Implement resolution for INNER mode

--- Miguel/Exercicios/cap_09/ex5_cadastro/main.c
+++ Miguel/Exercicios/cap_09/ex5_cadastro/main.c
@@ -7,12 +7,125 @@
 //     (vetor de estruturas)
 //      estutura: nome, end, cidade, estado, cep
 
+#define INNER_MODE
+
+#ifdef INNER_MODE
+
+#include <stdio.h>
+
+
+void clear_buffer() {
+    
+    while ( getchar() != '\n' );
+    return;
+}
+
+typedef struct {
+    
+    char name[25];
+    char end[5];
+    char city[20];
+    char state[3];
+    char cep[9];
+} Userdata;
+
+char DONE = 'n';
+
+void ask_handler(char *msg, char *ptr) {
+    
+    printf("%s", msg);
+    scanf("%s", ptr);
+    clear_buffer();
+    return;
+}
+
+void __1_insert(Userdata *users) {
+    
+    for (int i = 0; i < 4; i++) {
+        
+        Userdata *cur_user = (users + i);
+        
+        printf("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=");
+        
+        ask_handler("Digite seu nome:  ", cur_user->name);
+        ask_handler("Digite o estado:  ", cur_user->state);
+        ask_handler("Digite a cidade:  ", cur_user->city);
+        ask_handler("Digite o CEP:  ", cur_user->cep);
+        ask_handler("Digite o endereço:  ", cur_user->end);
+        
+        printf("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=");
+    }
+    return;
+}
+
+void __2_show(Userdata *users) {
+    
+    for (int i = 0; i < 4; i++) {
+        
+        Userdata cur_user = *(users + i);
+        
+        printf("-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=\n");
+        
+        printf("Nome: %s\n", cur_user.name);
+        printf("Cidade: %s\n", cur_user.city);
+        printf("Estado: %s\n", cur_user.state);
+        printf("CEP: %s\n", cur_user.cep);
+        printf("Endereço: %s\n", cur_user.end);
+        
+        printf("\n-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=\n");
+    }
+    return;
+}
+
+void __3_exit() {
+    
+    DONE = 's';
+}
+
+int ask_action() {
+    
+    int action;
+    
+    printf("--------------- Ações disponíveis: ------------\n");
+    printf("[ 1 ] Inserir dados\n");
+    printf("[ 2 ] Ver dados\n");
+    printf("[ 3 ] Sair\n");
+    
+    printf("---->>  ");
+    scanf("%d", &action);
+    clear_buffer();
+    return action;
+}
+
+
 int main() {
     
     char again = 'n';
     do {
         
+        Userdata users[4];
         
+        while (DONE == 'n') {
+                
+            int action = ask_action();
+            switch (action) {
+                
+                case 1:
+                __1_insert(&users[0]);
+                break;
+                
+                case 2:
+                __2_show(&users[0]);
+                break;
+                
+                case 3:
+                __3_exit();
+                break;
+                
+                default:
+                continue;
+            }
+        }
         // +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-
         printf("Deseja executar novamente ( s / n )  ");
         scanf("%c", &again);
@@ -20,3 +133,4 @@ int main() {
     } while (again == 's');
     return 0;
 }
+#endif // INNER_MODE
