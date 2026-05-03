defdb7a1d540ce7db080b3cd0d8c64724f5e11c8
miguel-webplace <minuz.code@gmail.com>
2026-05-03 17:07:25 -0300

    CAP 10 EX 1 => Implement resolution for INNER mode (not tested)

--- Miguel/Exercicios/cap_10/ex1_arq_manipula_linear/main.c
+++ Miguel/Exercicios/cap_10/ex1_arq_manipula_linear/main.c
@@ -5,10 +5,40 @@
 //     leia e imprima na tela todos os caracteres armazenados no
 //     arquivo (um por vezes).
 
+
+#include <stdio.h>
+
+
+
+
 int main() {
     
     char again = 'n';
     do {
+        
+        FILE *fptr;
+        fptr = fopen("caracteres.txt", "rw");
+        
+        if (fptr == NULL) {
+            printf("Erro ao abrir o arquivo\n");
+            exit(0);
+        }
+        
+        char ipt;
+        printf("--->>  ");
+        getchar();
+        while (ipt != '0') {
+            
+            fputc(ipt, fptr);
+            printf("--->>  ");
+            getchar(ipt, fptr);
+        }
+        fputc('\0', fptr);
+        
+        
+        fseek(fptr, 0, SEEK_SET);
+        
+        
         
         
         // +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-
