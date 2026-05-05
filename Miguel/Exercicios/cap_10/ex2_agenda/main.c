ea01834977281fb85f94c0335c152d41d28c561a
Miguel Andrade da Cruz <minuz.code@gmail.com>
2026-05-05 09:12:06 -0300

    CAP 10 EX 2
    => Implement resolution for INNER mode ( not tested )

--- Miguel/Exercicios/cap_10/ex2_agenda/main.c
+++ Miguel/Exercicios/cap_10/ex2_agenda/main.c
@@ -30,6 +30,29 @@ void clear_buffer() {
     return;
 }
 
+// --------------------|
+int DONE = 0;
+// --------------------|
+
+
+typedef struct {
+    
+    char name[26];
+    char phone[11];
+    int day;
+    int month;
+} Client;
+
+
+int ask_action() {
+
+    int ipt;
+    printf("Escolha sua ação:  ");
+    scanf("%d", &ipt);
+    return ipt;
+}
+
+
 void display_menu() {
     
     printf("[1] - inserir contato\n");
@@ -43,20 +66,25 @@ void display_menu() {
     return;
 }
 
+int strequal(char *ipt, char *compare) {
 
-typedef struct {
+    int i = 0;
+    while (
+        ipt[i] == compare[i] &&
+        ipt[i] != '\0'       &&
+        compare[i] != '\0'
+    ) {
+        i++;
+    }
     
-    char *name;
-    char *phone;
-    int day;
-    int month;
-} Client;
+    return ( ipt[i] == '\0' && compare[i] == '\0' );
+}
 
 
 void load_clients(Client *clients) {
     
     FILE *fptr;
-    fptr = fopen("../txts/agenda.txt", "w");
+    fptr = fopen("C:/Users/lab52/Desktop/Repos/fatec-conteudo/txts/agenda.txt", "w");
     if (fptr == NULL) {
         printf("Erro ao abrir o arquivo.");
         exit(0);
@@ -116,7 +144,7 @@ void display_client(Client *client) {
 }
 
 
-int DONE = 0;
+
 
 void __1_insert(Client *clients) {
     
@@ -125,6 +153,7 @@ void __1_insert(Client *clients) {
         if ( clients->name[0] == '^' ) {
             client_form(&clients[0]);
         }
+        i++;
     }
     printf("Arquivo cheio: sem espaço para novos clientes.\n");
     return;
@@ -154,7 +183,7 @@ void __3_search(Client *clients) {
     int i = 0;
     while (i < 4) {
         
-        int is_equal = strequal(ipt, clients->name);
+        int is_equal = strequal(ipt, clients[i].name);
         if (is_equal) {
             display_client(&clients[i]);
         }
@@ -163,9 +192,82 @@ void __3_search(Client *clients) {
 }
 
 
-void __4_(Client *clients) {
+void __4_list(Client *clients) {
     
+    char ipt;
+    printf("Insira a primeira letra do nome do cliente:  ");
+    ipt = getchar();
     
+    for (int i = 0; i < 4; i++) {
+        
+        if ( ipt == clients[i].name[0] ) {
+            display_client(&clients[i]);
+        }
+    }
+    return;
+}
+
+
+void __5_display_month(Client *clients) {
+    
+    int current_month;
+    printf("Qual é o mês da data de hoje? --->>  ");
+    scanf("%d", &current_month);
+    
+    for (int i = 0; i < 4; i++) {
+        
+        if ( current_month == clients[i].month ) {
+            display_client(&clients[i]);
+        }
+    }
+    
+    return;
+}
+
+
+void __6_edit(Client *clients) {
+    
+    char ipt[25];
+    printf("Pesquise pelo nome completo do cliente:  ");
+    scanf("%s", ipt);
+    clear_buffer();
+    
+    int i = 0;
+    while (i < 4) {
+        
+        int is_equal = strequal(ipt, clients[i].name);
+        if ( is_equal ) {
+            client_form(&clients[i]);
+            break;
+        }
+    }
+    return;
+}
+
+
+void __7_delete(Client *clients) {
+    
+    char ipt[25];
+    printf("Pesquise pelo nome completo do cliente:  ");
+    scanf("%s", ipt);
+    clear_buffer();
+    
+    int i = 0;
+    while (i < 4) {
+        
+        int is_equal = strequal(ipt, clients[i].name);
+        if ( is_equal ) {
+            clients[i].name[0] == '^';
+            break;
+        }
+    }    
+    return;
+}
+
+
+void __8_exit() {
+
+    DONE = 1;
     return;
 }
 
@@ -173,49 +275,52 @@ int main() {
     
     char again = 'n';
     
-    Client clients[4];
-    load_clients(clients);
     do {
         
-        display_menu();
-        int action = ask_action();
-        
-        switch (action) {
+        Client clients[4];
+        load_clients(clients);
+        do {
             
-            case 1:
-                __1_();
-            break;
+            display_menu();
+            int action = ask_action();
             
-            case 2:
-                __1_();
-            break;
-            
-            case 3:
-                __1_();
-            break;
-            
-            case 4:
-                __1_();
-            break;
-            
-            case 5:
-                __1_();
-            break;
-            
-            case 6:
-                __1_();
-            break;
-            
-            case 7:
-                __1_();
-            break;
-            
-            case 8:
-                __1_();
-            break;
-        }
-    } while (!DONE);
-    store_clients(clients);
+            switch (action) {
+                
+                case 1:
+                    __1_insert(clients);
+                break;
+                
+                case 2:
+                    __2_list(clients);
+                break;
+                
+                case 3:
+                    __3_search(clients);
+                break;
+                
+                case 4:
+                    __4_list(clients);
+                break;
+                
+                case 5:
+                    __5_display_month(clients);
+                break;
+                
+                case 6:
+                    __6_edit(clients);
+                break;
+                
+                case 7:
+                    __7_delete(clients);
+                break;
+                
+                case 8:
+                    __8_exit();
+                break;
+            }
+        } while (!DONE);
+
+        store_clients(clients);
         // +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-
         printf("Deseja executar novamente ( s / n )  ");
         scanf("%c", &again);
