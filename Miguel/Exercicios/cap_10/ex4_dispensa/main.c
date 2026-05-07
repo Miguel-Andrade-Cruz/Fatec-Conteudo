660d8bf37b3fe3a4a5537846cf6cc92c622bf5a6
miguel-webplace <minuz.code@gmail.com>
2026-05-06 21:45:33 -0300

    CAP 10 EX 4
    => Resolve I/O and segfault errors [NOT WORKING: Menu options 5, 6 and 7
    ]

--- Miguel/Exercicios/cap_10/ex4_dispensa/main.c
+++ Miguel/Exercicios/cap_10/ex4_dispensa/main.c
@@ -20,7 +20,7 @@
 #include <stdlib.h>
 
 // -----------------------|
-#define DATA_FILE "dispensa.txt"
+#define DATA_FILE "../txts/dispensa.txt"
 int DONE = 0;
 // -----------------------|
 
@@ -37,52 +37,59 @@ typedef struct {
     int stock;
 } Product;
 
-void load_product(Product *prodct) {
-
-    FILE *fptr;
-    fptr = fopen(DATA_FILE, "r");
-    if (fptr == NULL) {
-        printf("Erro ao abrir o arquivo.\n");
-        exit(0);
+int strequal(char *ipt, char *compare) {
+    
+    int i = 0;
+    while (
+        ipt[i] == compare[i] &&
+        ipt[i] != '\0'       &&
+        compare[i] != '\0'
+    ) {
+        i++;
     }
     
-    size_t sucess = fread(product, sizeof(Product), 1, fptr);
-    if (!sucess) product->sku = 0000;
-    fclose(fptr);
-    return;
+    return ( ipt[i] == '\0' && compare[i] == '\0' );
 }
 
 
-void store_product(Product *product) {
-
-    FILE *fptr;
-    fopen(DATA_FILE, "w");
-    if (fptr == NULL) {
-        printf("Erro ao abrir o arquivo.\n");
-        exit(0);
-    }
+void display_menu() {
     
-    fwrite(product, sizeof(Product), 1, fptr);
-    fclose(fptr);
+    printf("[ 1 ] - incluir produtos\n");
+    printf("[ 2 ] - listar todos os produtos\n");
+    printf("[ 3 ] - pesquisar uma mercadoria pela descricao\n");
+    printf("[ 4 ] - listar os produtos nao disponiveis.\n");
+    printf("[ 5 ] - alterar a quantidade atual\n");
+    printf("[ 6 ] - alterar produto\n");
+    printf("[ 7 ] - excluir produto\n");
+    printf("[ 8 ] - sair\n");
     return;
+}
+
+int ask_action() {
+    
+    int action;
+    printf("--->>  ");
+    scanf("%d", &action);
+    clear_buffer();
+    return action;
 }
 
 void ask_product(Product *product) {
-
+    
     printf("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=\n");
     
     printf("Descrição:  ");
     scanf("%s", product->description);
     clear_buffer();
-
+    
     printf("Código (SKU):  ");
     scanf("%s", product->sku);
     clear_buffer();
-
+    
     printf("Estoque:  ");
     scanf("%d", &product->stock);
     clear_buffer();
-
+    
     printf("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=\n");
     return;
 }
@@ -99,36 +106,194 @@ void display_product(Product *product) {
 
 void __1_insert(Product *product) {
     
+    FILE *fptr;
+    fptr = fopen(DATA_FILE, "a+");
+    if (fptr == NULL) {
+        printf("Erro ao abrir o arquivo.\n");
+        exit(0);
+    }
+    char again = 's';
+    while ( again == 's' ) {
+        size_t sucess = fread(product, sizeof(Product), 1, fptr);
+        if (sucess != 0) {
+            if (product->sku[0] == '\0') {
+                fseek(fptr, -sizeof(Product), SEEK_CUR);
+            } else {
+                continue;
+            }
+        }
+        ask_product(product);
+        fwrite(product, sizeof(Product), 1, fptr);
+        printf("Continuar? [ s / n ] --->>  ");
+        again = getchar();
+    }
+    fclose(fptr);
     return;
 }
 
 void __2_list(Product *product) {
     
+    FILE *fptr;
+    fptr = fopen(DATA_FILE, "r");
+    if (fptr == NULL) {
+        printf("Erro ao abrir o arquivo.\n");
+        exit(0);
+    }
+    
+    int has_records = 0;
+    while (1) {
+        
+        size_t sucess = fread(product, sizeof(Product), 1, fptr);
+        if (product->sku[0] == '\0') {
+            has_records = 1;
+            continue;
+        }
+        if (feof(fptr)) break;
+        display_product(product);
+    }
+    if (!has_records) printf("Não existem produtos na dispensa.\n");
+    fclose(fptr);
     return;
 }
 
 void __3_search(Product *product) {
     
+    char ipt[26];
+    printf("Pesquise pelo nome do produto:  ");
+    scanf("%s", ipt);
+    clear_buffer();
+    
+    FILE *fptr;
+    fptr = fopen(DATA_FILE, "r");
+    int found = 0;
+    while (1) {
+        
+        fread(product, sizeof(Product), 1, fptr);
+        int matched = strequal(ipt, product->description);
+        if (matched) {
+            found = 1;
+            display_product(product);
+            break;
+        } else if (feof(fptr)) {
+            break;
+        }
+    }
+    if (!found) printf("'%s' não existe na dispensa.\n", ipt);
+    fclose(fptr);
     return;
 }
 
-void __4_available(Product *product) {
+void __4_not_available(Product *product) {
     
+    FILE *fptr;
+    fptr = fopen(DATA_FILE, "r");
+    int found = 0;
+    while (1) {
+        
+        fread(product, sizeof(Product), 1, fptr);
+        if (feof(fptr)) break;
+        if (product->stock == 0) {
+            found = 1;
+            display_product(product);
+        }
+    }
+    if (!found) printf("Não há estoque vazio.\n");
+    fclose(fptr);
     return;
 }
 
 void __5_change_qtd(Product *product) {
+    
+    char ipt[26];
+    printf("Pesquise pelo nome do produto:  ");
+    scanf("%s", ipt);
+    clear_buffer();
+    
+    FILE *fptr;
+    fptr = fopen(DATA_FILE, "w+");
+    int found = 0;
+    while (1) {
+        
+        fread(product, sizeof(Product), 1, fptr);
+        if (feof(fptr)) break;
+        
+        int matched = strequal(ipt, product->description);
+        if (matched) {
+            
+            int found = 1;
+            printf("Nova quantidade:  ");
+            scanf("%d", &product->stock);
+            clear_buffer();
+            
+            fseek(fptr, -sizeof(Product), SEEK_CUR);
+            fwrite(product, sizeof(Product), 1, fptr);
+            break;
+        }
+    }
+    if (!found) printf("Produto '%s' não encontrado.\n", ipt);
+    fclose(fptr);
     
     return;
 }
 
 void __6_edit(Product *product) {
     
+    char ipt[26];
+    printf("Pesquise pelo nome do produto:  ");
+    scanf("%s", ipt);
+    clear_buffer();
+    
+    FILE *fptr;
+    fptr = fopen(DATA_FILE, "a+");
+    int found = 0;
+    while (1) {
+        
+        fread(product, sizeof(Product), 1, fptr);
+        if (feof(fptr)) break;
+        
+        int matched = strequal(ipt, product->description);
+        if (matched) {
+            
+            int found = 1;
+            ask_product(product);
+            
+            fseek(fptr, -sizeof(Product), SEEK_CUR);
+            fwrite(product, sizeof(Product), 1, fptr);
+            break;
+        }
+    }
+    if (!found) printf("Produto '%s' não encontrado.\n", ipt);
+    fclose(fptr);
     return;
 }
 
 void __7_delete(Product *product) {
     
+    char ipt[26];
+    printf("Pesquise pelo nome do produto:  ");
+    scanf("%s", ipt);
+    clear_buffer();
+    
+    FILE *fptr;
+    fptr = fopen(DATA_FILE, "a+");
+    int found = 0;
+    while (1) {
+        
+        fread(product, sizeof(Product), 1, fptr);
+        if (feof(fptr)) break;
+        
+        int matched = strequal(ipt, product->description);
+        if (matched) {
+            
+            found = 1;
+            product->sku[0] = '\0';
+            fseek(fptr, -sizeof(Product), SEEK_CUR);
+            fwrite(product, sizeof(Product), 1, fptr);
+            break;
+        }
+    }
+    if (!found) printf("Produto '%s' não encontrado.\n", ipt);
+    fclose(fptr);
     return;
 }
 
@@ -146,7 +311,7 @@ int main() {
         DONE = 0;
         
         Product prod_buff;
-
+        
         do {
             display_menu();
             
@@ -154,35 +319,35 @@ int main() {
             switch (action) {
                 
                 case 1:
-                __1_();
+                __1_insert(&prod_buff);
                 break;
-
+                
                 case 2:
-                __2_();
+                __2_list(&prod_buff);
                 break;
-        
+                
                 case 3:
-                __3_();
+                __3_search(&prod_buff);
                 break;
-
+                
                 case 4:
-                __4_();
+                __4_not_available(&prod_buff);
                 break;
-
+                
                 case 5:
-                __5_();
+                __5_change_qtd(&prod_buff);
                 break;
-
+                
                 case 6:
-                __6_();
+                __6_edit(&prod_buff);
                 break;
-
+                
                 case 7:
-                __7_();
+                __7_delete(&prod_buff);
                 break;
-
+                
                 case 8:
-                __8_();
+                __8_exit();
                 break;
             }
         } while (!DONE);
