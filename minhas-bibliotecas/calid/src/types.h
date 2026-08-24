f25307bdb2eef20714b487025c06b57038edc128
miguel-webplace <minuz.code@gmail.com>
2026-08-23 22:34:47 -0300

    Create the types for the new interface

--- minhas-bibliotecas/calid/src/types.h
+++ minhas-bibliotecas/calid/src/types.h
@@ -1,39 +1,64 @@
 
-typedef enum {
-    
-    INT,
-    CHAR,
-    FLOAT,
-    STR
-} Type;
 
-typedef union {
-    
-    int v_int;
-    char v_char;
-    float v_float;
-    char *v_str;
-} Generic;
-
-typedef struct{
-    
-    Type type;
-    Generic value;
-} Any;
-
-typedef int ( *Checkit )(Any, Any);
+typedef int ( *Validator )(int, int); // TODO: Adicionar novo tipo para o tipo de variavel
 
 
 typedef struct {
-    
-    Checkit verifier;
-    Any bound;
-} Filter;
+    Validator *verify;
+    int value; // TODO: Adicionar novo tipo para o tipo de variavel
+} Checklist;
 
 
 typedef struct {
-    
-    char *descrp;
-    Any ipt;
-    Filter *filters;
-} Field;
+    char *question;
+    char *question_err;
+    int *destiny_ptr; // TODO: Adicionar novo tipo para o tipo de variavel
+    Checklist *checklist;
+} Form;
+
+
+
+
+
+
+
+
+
+// typedef enum {
+
+//     INT,
+//     CHAR,
+//     FLOAT,
+//     STR
+// } Type;
+
+// typedef union {
+
+//     int v_int;
+//     char v_char;
+//     float v_float;
+//     char *v_str;
+// } Generic;
+
+// typedef struct{
+
+//     Type type;
+//     Generic value;
+// } Any;
+
+// typedef int ( *Checkit )(Any, Any);
+
+
+// typedef struct {
+
+//     Checkit verifier;
+//     Any bound;
+// } Filter;
+
+
+// typedef struct {
+
+//     char *descrp;
+//     Any ipt;
+//     Filter *filters;
+// } Field;
