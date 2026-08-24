9a7f4b138c9bb523f9a424dbb764ce48f9706655
miguel-webplace <minuz.code@gmail.com>
2026-08-23 22:34:25 -0300

    - Rename folder and file for suitable purpose of examples
    - Create new interface for the lib

---
+++ minhas-bibliotecas/calid/src/examples/age_filter.c
@@ -0,0 +1,33 @@
+#include "../scan.c"
+
+
+// Example of filling a age field
+
+int main() {
+
+
+    // Variable and pointer to hold the data
+    int user_age, *ptr_user_age;
+
+
+    Form form = create_form();
+
+    // Create a stack in dynamic memory for all the validations
+    Checklist age_filters = create_checklist();
+
+    // Allocating (or reallocating) memory for the filters
+    add_check(age_filters, greater_than(18));
+    add_check(age_filters, less_than(70));
+
+    add_field(
+        &form,
+        "Digite a sua idade: ",
+        "por favor, insira uma idade válida: ",
+        &ptr_user_age,
+        &age_filters
+    );
+
+    apply(form);
+
+    return 0;
+}
